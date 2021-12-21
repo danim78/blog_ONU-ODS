a20c604b0b6ce4b94e41c59025ea7c6ef5287753
b4ngi <ignaciobenetucci@gmail.com>
2021-12-20 23:42:21 -0300

    formato a los botones de busqueda

--- apps/post/forms.py
+++ apps/post/forms.py
@@ -19,12 +19,14 @@ class BusquedaPost(forms.Form):
             ("nuevo", "Nuevo"))
         ))
         
-    orden = forms.ChoiceField(choices=ORDER_OPCIONES, required = False, initial="nuevo")
+    orden = forms.ChoiceField(choices=ORDER_OPCIONES, widget=forms.Select, required = False, initial="nuevo")
     categoria = forms.ModelChoiceField(queryset=Categoria.objects.all(), widget=forms.Select, required = False)
 
     def __init__(self, *args, **kwargs):
         super(BusquedaPost, self).__init__(*args, **kwargs)
         self.fields["titulo"].widget.attrs.update({'class' : 'form-control form-control-lg', 'placeholder' : ' Titulo', 'type' : 'text', 'aria-label':'.form-control-lg example' , 'class' : 'h-100 d-inline-block'})
+        self.fields["orden"].widget.attrs.update({'class':"btn btn-outline-primary dropdown-toggle", 'type':"button", 'data-bs-toggle':"dropdown", 'aria-expanded':"false"})
+        self.fields["categoria"].widget.attrs.update({'class':"btn btn-outline-primary dropdown-toggle", 'type':"button", 'data-bs-toggle':"dropdown", 'aria-expanded':"false"})
 
 class ComentarioForm(forms.ModelForm):
     class Meta:
