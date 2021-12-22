0652bd63c3b6dca8cd15501448366973486c5d63
PabloColetti <pablo01pablo011@gmail.com>
2021-12-22 06:35:20 -0300

    Estilos a ultimos templates

--- apps/post/forms.py
+++ apps/post/forms.py
@@ -7,6 +7,16 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('titulo', 'contenido', 'permitir_comentarios', 'categoria', 'imagen')
+    
+    categoria = forms.ModelChoiceField(queryset=Categoria.objects.all(), widget=forms.Select, empty_label="Seleccionar categoria")
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields["titulo"].widget.attrs.update({'class' : 'form-control', 'placeholder' : 'Escriba el titulo de su nuevo post aqui.', 'type' : 'text' , 'id' : 'titulo_msj'})
+        self.fields["contenido"].widget.attrs.update({'class' : 'form-control', 'placeholder' : 'Escriba su nuevo post aqui.', 'type' : 'text' , 'id' : 'contenido_msj'})
+        self.fields["permitir_comentarios"].widget.attrs.update({'class' : 'form-check form-switch form-check-input', 'type' : 'checkbox' , 'type' : 'checkbox' , 'id' : 'permitir_comentarios_msj'})
+        self.fields["categoria"].widget.attrs.update({'select class':'form-select form-select'})
+        self.fields["imagen"].widget.attrs.update({'class':'btn-sm'})
 
 class BusquedaPost(forms.Form):
     titulo = forms.CharField(max_length=30, required = False)
@@ -21,7 +31,7 @@ class BusquedaPost(forms.Form):
         ))
         
     orden = forms.ChoiceField(choices=ORDER_OPCIONES, widget=forms.Select, required = False, initial="titulo")
-    categoria = forms.ModelChoiceField(queryset=Categoria.objects.all(), widget=forms.Select, required = False, empty_label="Categoria")
+    categoria = forms.ModelChoiceField(queryset=Categoria.objects.all(), widget=forms.Select, required = False, empty_label="Seleccionar categoria")
 
     def __init__(self, *args, **kwargs):
         super(BusquedaPost, self).__init__(*args, **kwargs)
@@ -36,4 +46,4 @@ class ComentarioForm(forms.ModelForm):
     
     def __init__(self, *args, **kwargs):
         super(ComentarioForm, self).__init__(*args, **kwargs)
-        self.fields["mensaje"].widget.attrs.update({'class' : 'form-control', 'placeholder' : 'Escriba su comentario', 'type' : 'text'})
+        self.fields["mensaje"].widget.attrs.update({'class' : 'form-control', 'placeholder' : 'Escriba un nuevo comentario aqui.', 'type' : 'text'})
